ffe9ef6d95c50fb57a4011a35c87f219f1d19f44
ai ai <1875224484@qq.com>
2025-10-12 22:25:16 +0800

    尝试二次提交

--- csdnauth/views.py
+++ csdnauth/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    """用户登录"""
+    """用户登录接口"""
     return render(request,'login.html')
 
 def regist(request):
     """
-    用户注册
+    用户注册接口
     """
     return render(request,'regist.html')
